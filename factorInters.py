770e3926543b358b89af9d7cb94aaa23fd147565
gghh <gghh@tosh>
2012-03-25 06:38:04 +0200

    testing cleaning

--- factorInters.py
+++ factorInters.py
@@ -8,6 +8,8 @@ dir(p)
 type(p)
 print p
 
+# convert ensemble name to ID thru a global dictionary getID
+
 def choose_n(n, srcList):
     if n == 0:
         return [[]]
@@ -18,13 +20,17 @@ def choose_n(n, srcList):
                        choose_n(n-1, srcList[:cnt] + srcList[(cnt+1):]))
         return out
 
-choose_n(0, ['a', 'b', 'c'])
-
-choose_n(1, ['a', 'b', 'c'])
-
-choose_n(2, ['a', 'b', 'c'])
-
-choose_n(3, ['a', 'b', 'c'])
+assert(choose_n(0, ['a', 'b', 'c']) == [[]])
+assert(choose_n(1, ['a', 'b', 'c']) == [['a'], ['b'], ['c']])
+assert(choose_n(2, ['a', 'b', 'c']) ==
+       [['a', 'b'], ['a', 'c'], ['b', 'a'],
+        ['b', 'c'], ['c', 'a'], ['c', 'b']])
+# next one is lame: I get permutations (wanted singleton, but no big deal.
+# cleaning with a later function
+assert(choose_n(3, ['a', 'b', 'c']) ==
+       [['a', 'b', 'c'], ['a', 'c', 'b'],
+        ['b', 'a', 'c'], ['b', 'c', 'a'],
+        ['c', 'a', 'b'], ['c', 'b', 'a']])
 
 def allChoices(srcList):
     out = []
@@ -32,7 +38,17 @@ def allChoices(srcList):
         out += choose_n(i, srcList)
     return out
 
-allChoices(['a', 'b', 'c'])
+assert(allChoices(['a', 'b', 'c']) ==
+       [[],
+
+        ['a'], ['b'], ['c'],
+
+        ['a', 'b'], ['a', 'c'], ['b', 'a'],
+        ['b', 'c'], ['c', 'a'], ['c', 'b'],
+
+        ['a', 'b', 'c'], ['a', 'c', 'b'],
+        ['b', 'a', 'c'], ['b', 'c', 'a'],
+        ['c', 'a', 'b'], ['c', 'b', 'a']])
 
 def remDupes(listlist):
     out = []
@@ -42,7 +58,10 @@ def remDupes(listlist):
             out.append(li)
     return out
 
-remDupes(allChoices(['a', 'b', 'c']))
+assert(remDupes(allChoices(['a', 'b', 'c'])) ==
+       [[], ['a'], ['b'], ['c'],
+        ['a', 'b'], ['a', 'c'], ['b', 'c'],
+        ['a', 'b', 'c']])
 
 def remEmpty(lis):
     for cnt, elem in enumerate(lis):
@@ -50,14 +69,42 @@ def remEmpty(lis):
             return lis[:cnt] + lis[(cnt+1):]
     raise exceptions.Exception('empty non found')
 
-remEmpty(remDupes(allChoices(['a', 'b', 'c'])))
+# empty dies alone
+assert(remEmpty(remDupes(allChoices(['a', 'b', 'c']))) ==
+       [['a'], ['b'], ['c'],
+        ['a', 'b'], ['a', 'c'], ['b', 'c'],
+        ['a', 'b', 'c']])
+
+def getChildren(node, allPaths):
+    # NODE is a partial path, starting from the
+    # all-intersected ensebles.
+    # ALLPATHS are complete paths, from one end
+    # to the other of the lattice. You get them via
+    # genMap.
+    out = []
+    for path in allPaths:
+        if node2str(path).startswith(node2str(node)):
+            for cnt, endpoint in enumerate(path):
+                if not endpoint == node[cnt]:
+                    # first endpoint who differs,
+                    # that the repr of a kid. Put in the backpack.
+                    out.append(path[:cnt])
+                    break
+    return out
+
+def node2str(node):
+    # node is a list of IDs indentifying the node from the bottom
+    # of the lattice. Well, the bottom on the diagram I drew
+    # on the witheboard :-)
+    # Note: they're numerical IDs, so no risk of clashes with char '/'
+    return '/'.join(map(str, node))
 
 
-def genMap(setNames):
-    return [p for p in itertools.permutations(setNames)]
+def genMap(IDList):
+    return [p for p in itertools.permutations(IDList)]
 
 paths = genMap(['A', 'B', 'C', 'D'])
-slice = namedtuple('slice', ['node', 'card', 'addiInfo'])
+slice = namedtuple('slice', ['node', 'cardi', 'addiInfo'])
 
 def facto(inters, subUns, interMap):
     
